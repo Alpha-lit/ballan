9bbb1a716cecfa9397d8c50edbf6568aedc40d99
Alpha-lit <blak5naut@gmail.com>
2025-12-17 16:59:02 +0300

    q

--- ballan_travel/settings.py
+++ ballan_travel/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-896lcq=)=^p_%!%9k^rk*fhegwigmm&_nna(o=-6ets0s-=e2r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-8d392.up.railway.app']
 
 
 # Application definition
